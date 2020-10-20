03429c30a3315d801dd71c0860f41d47eec41beb
Jacky Wu <jackywu20050@allion.com.tw>
2020-10-20 18:01:29 +0800

    Add HtmlToExcel
    新增專案

---
+++ HtmlToExcel/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace HtmlToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.htmltextBox = new System.Windows.Forms.TextBox();
+            this.exceltextBox = new System.Windows.Forms.TextBox();
+            this.scripttextBox = new System.Windows.Forms.TextBox();
+            this.htmlcolumntextBox = new System.Windows.Forms.TextBox();
+            this.scriptbutton = new System.Windows.Forms.Button();
+            this.excelbutton = new System.Windows.Forms.Button();
+            this.hmtlbutton = new System.Windows.Forms.Button();
+            this.runbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // htmltextBox
+            // 
+            this.htmltextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.htmltextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.htmltextBox.Location = new System.Drawing.Point(91, 133);
+            this.htmltextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.htmltextBox.Name = "htmltextBox";
+            this.htmltextBox.ReadOnly = true;
+            this.htmltextBox.Size = new System.Drawing.Size(547, 27);
+            this.htmltextBox.TabIndex = 0;
+            this.htmltextBox.DoubleClick += new System.EventHandler(this.htmltextBox_DoubleClick);
+            // 
+            // exceltextBox
+            // 
+            this.exceltextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.exceltextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.exceltextBox.Location = new System.Drawing.Point(92, 84);
+            this.exceltextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.exceltextBox.Name = "exceltextBox";
+            this.exceltextBox.ReadOnly = true;
+            this.exceltextBox.Size = new System.Drawing.Size(546, 27);
+            this.exceltextBox.TabIndex = 1;
+            this.exceltextBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.exceltextBox_MouseDoubleClick);
+            // 
+            // scripttextBox
+            // 
+            this.scripttextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.scripttextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scripttextBox.Location = new System.Drawing.Point(92, 11);
+            this.scripttextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.scripttextBox.Name = "scripttextBox";
+            this.scripttextBox.ReadOnly = true;
+            this.scripttextBox.Size = new System.Drawing.Size(546, 27);
+            this.scripttextBox.TabIndex = 2;
+            this.scripttextBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.scripttextBox_MouseDoubleClick);
+            // 
+            // htmlcolumntextBox
+            // 
+            this.htmlcolumntextBox.Location = new System.Drawing.Point(111, 174);
+            this.htmlcolumntextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.htmlcolumntextBox.Name = "htmlcolumntextBox";
+            this.htmlcolumntextBox.Size = new System.Drawing.Size(50, 27);
+            this.htmlcolumntextBox.TabIndex = 3;
+            this.htmlcolumntextBox.Text = "7";
+            this.htmlcolumntextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.htmlcolumntextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // scriptbutton
+            // 
+            this.scriptbutton.Location = new System.Drawing.Point(6, 7);
+            this.scriptbutton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.scriptbutton.Name = "scriptbutton";
+            this.scriptbutton.Size = new System.Drawing.Size(76, 37);
+            this.scriptbutton.TabIndex = 4;
+            this.scriptbutton.Text = "Script";
+            this.scriptbutton.UseVisualStyleBackColor = true;
+            this.scriptbutton.Click += new System.EventHandler(this.scriptbutton_Click);
+            // 
+            // excelbutton
+            // 
+            this.excelbutton.Location = new System.Drawing.Point(6, 77);
+            this.excelbutton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.excelbutton.Name = "excelbutton";
+            this.excelbutton.Size = new System.Drawing.Size(76, 37);
+            this.excelbutton.TabIndex = 5;
+            this.excelbutton.Text = "Excel";
+            this.excelbutton.UseVisualStyleBackColor = true;
+            this.excelbutton.Click += new System.EventHandler(this.excelbutton_Click);
+            // 
+            // hmtlbutton
+            // 
+            this.hmtlbutton.Location = new System.Drawing.Point(5, 126);
+            this.hmtlbutton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.hmtlbutton.Name = "hmtlbutton";
+            this.hmtlbutton.Size = new System.Drawing.Size(76, 37);
+            this.hmtlbutton.TabIndex = 6;
+            this.hmtlbutton.Text = "HTML";
+            this.hmtlbutton.UseVisualStyleBackColor = true;
+            this.hmtlbutton.Click += new System.EventHandler(this.hmtlbutton_Click);
+            // 
+            // runbutton
+            // 
+            this.runbutton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.runbutton.Location = new System.Drawing.Point(548, 167);
+            this.runbutton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.runbutton.Name = "runbutton";
+            this.runbutton.Size = new System.Drawing.Size(94, 37);
+            this.runbutton.TabIndex = 7;
+            this.runbutton.Text = "Run";
+            this.runbutton.UseVisualStyleBackColor = true;
+            this.runbutton.Click += new System.EventHandler(this.runbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Html Column";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.scriptbutton);
+            this.panel1.Controls.Add(this.runbutton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.htmlcolumntextBox);
+            this.panel1.Controls.Add(this.scripttextBox);
+            this.panel1.Controls.Add(this.excelbutton);
+            this.panel1.Controls.Add(this.hmtlbutton);
+            this.panel1.Controls.Add(this.htmltextBox);
+            this.panel1.Controls.Add(this.exceltextBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 208);
+            this.panel1.TabIndex = 9;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AutoWordSelection = true;
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 208);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(652, 448);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Close;
+            this.comboBox1.Location = new System.Drawing.Point(92, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(546, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Sheet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 656);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Result 回填";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox htmltextBox;
+        private System.Windows.Forms.TextBox exceltextBox;
+        private System.Windows.Forms.TextBox scripttextBox;
+        private System.Windows.Forms.TextBox htmlcolumntextBox;
+        private System.Windows.Forms.Button scriptbutton;
+        private System.Windows.Forms.Button excelbutton;
+        private System.Windows.Forms.Button hmtlbutton;
+        private System.Windows.Forms.Button runbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HtmlToExcel/Form1.cs
@@ -0,0 +1,197 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HtmlToExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 選擇Excel腳本並讀取Sheet到ComboBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void scriptbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Title = "Select Script File"
+            };
+            string path = Directory.GetCurrentDirectory() + @"\Script";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            dialog.InitialDirectory = Directory.GetCurrentDirectory() + @"\Script";
+            dialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                scripttextBox.Text = dialog.FileName;
+                FileInfo file = new FileInfo(dialog.FileName);
+                comboBox1.Items.Clear();
+                using (ExcelPackage excelPackage = new ExcelPackage(file))
+                {
+                    foreach(ExcelWorksheet excelWorksheet in excelPackage.Workbook.Worksheets)
+                    {
+                        comboBox1.Items.Add(excelWorksheet.Name);
+                    }
+                    if(comboBox1.Items.Count>0) //有工作表就顯示
+                        comboBox1.SelectedItem = comboBox1.Items[0];
+                }
+            }
+        }
+
+        /// <summary>
+        /// 點2下打開Script
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void scripttextBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (File.Exists(scripttextBox.Text))
+            {
+                System.Diagnostics.Process.Start(scripttextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("找不到Script\n"+scripttextBox.Text);
+            }
+        }
+
+        /// <summary>
+        /// 選擇要寫入的Excel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void excelbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Title = "Select Script File"
+            };
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                exceltextBox.Text = dialog.FileName;
+            }
+        }
+
+        /// <summary>
+        /// 點2下開啟要寫入的Excel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exceltextBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (File.Exists(exceltextBox.Text))
+            {
+                System.Diagnostics.Process.Start(exceltextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("找不到Excel\n" + exceltextBox.Text);
+            }
+        }
+
+        /// <summary>
+        /// 選擇要讀取的HTML
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void hmtlbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select Html File";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dialog.Filter = "HTML|*.html";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                htmltextBox.Text = dialog.FileName;
+        }
+
+        /// <summary>
+        /// 點2下打開HTML
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void htmltextBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (File.Exists(htmltextBox.Text))
+            {
+                System.Diagnostics.Process.Start(htmltextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("找不到HTML\n" + htmltextBox.Text);
+            }
+        }
+
+        /// <summary>
+        /// 限定輸入數字
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back)))
+                e.Handled = true;
+        }
+
+        /// <summary>
+        /// 點Run執行寫入
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void runbutton_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(scripttextBox.Text))
+            {
+                MessageBox.Show("找不到Script\n" + htmltextBox.Text);
+                return;
+            }
+            if (!File.Exists(exceltextBox.Text))
+            {
+                MessageBox.Show("找不到Excel\n" + exceltextBox.Text);
+                return;
+            }
+            if (!File.Exists(htmltextBox.Text))
+            {
+                MessageBox.Show("找不到HTML\n" + htmltextBox.Text);
+                return;
+            }
+            if(htmlcolumntextBox.Text==""|| int.Parse(htmlcolumntextBox.Text)<0)
+            {
+                MessageBox.Show("請輸入正確Html Column");
+                return;
+            }
+            //先檢查再寫入
+
+
+        }
+
+        /// <summary>
+        /// 開始讀檔寫入
+        /// </summary>
+        /// <param name="script"></param>
+        /// <param name="excel"></param>
+        /// <param name="html"></param>
+        private void WriteToExcel(FileInfo script,FileInfo excel ,FileInfo html)
+        {
+
+        }
+
+    }
+}
